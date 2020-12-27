961a9abd8ea8390a24dc9c8d04e084da13a1e9e9
Gongon2089 <gonzaloyubero@gmail.com>
2020-12-27 19:54:33 +0100

    mp4 to mp3 conversion and mp4 deletion successful

--- downloader.py
+++ downloader.py
@@ -1,10 +1,16 @@
 import os
+import time
 from moviepy.editor import *
 import pytube as p
 
 
 def download(URL, name):
-    for s in p.YouTube(URL).streams:
-        print(s)
-    stream = p.YouTube(URL).streams.get_by_itag(140)
+
+    stream = p.YouTube(URL).streams.get_by_itag(22)
     stream.download(filename=name)
+
+    video = VideoFileClip(filename=f"{name}.mp4")
+    video.audio.write_audiofile(os.path.join(os.getcwd(), f"{name}.mp3"))
+    video.close()
+    time.sleep(5)
+    os.remove(f"{name}.mp4")
