dbd4a217fa657f1ed064b05363e5e7c0d363c75d
Gongon2089 <gonzaloyubero@gmail.com>
2020-12-27 18:56:31 +0100

    input now clears after submiting

--- View.py
+++ View.py
@@ -2,7 +2,7 @@ import PySimpleGUI as sg
 
 
 def showWindow():
-    layout = [[sg.Text("Enter URL")], [sg.Input()] ,[sg.Button("SUBMIT")]]
+    layout = [[sg.Text("Enter URL")], [sg.Input(key='-INPUT-')], [sg.Text(size=(40,1), key='-OUTPUT-')], [sg.Button("SUBMIT")]]
 
     # Create the window
     window = sg.Window("Input", layout)
@@ -13,7 +13,11 @@ def showWindow():
         # End program if user closes window or
         # presses the OK button
         if event == "SUBMIT":
-            print("hey")
+            window['-OUTPUT-']('Downloading...')
+
+            window['-OUTPUT-']('Done!')
+
+            window['-INPUT-']('')
         if event == sg.WIN_CLOSED:
             break
 
