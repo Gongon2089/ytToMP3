4fe50c52220f08ea1f2605ac1aa194caf71e696c
Gongon2089 <gonzaloyubero@gmail.com>
2020-12-27 21:07:02 +0100

    now accepts empty default name

--- downloader.py
+++ downloader.py
@@ -2,15 +2,32 @@ import os
 import time
 from moviepy.editor import *
 import pytube as p
+import getpass
 
 
 def download(URL, name):
 
-    stream = p.YouTube(URL).streams.get_by_itag(22)
+    ytObj = p.YouTube(URL)
+    stream = ytObj.streams.get_by_itag(22)
+    if stream is None:
+        stream = ytObj.streams.get_by_itag(18)
     stream.download(filename=name)
+    enter = False
+    if name == "":
+        files = os.listdir()
+        for f in files:
+            if ".mp4" in f:
+                enter = True
+                name = f
 
-    video = VideoFileClip(filename=f"{name}.mp4")
-    video.audio.write_audiofile(os.path.join(os.getcwd(), f"{name}.mp3"))
+    if not enter:
+        video = VideoFileClip(filename=f"{name}.mp4")
+    else:
+        video = VideoFileClip(filename=name)
+    video.audio.write_audiofile(f'C:\\Users\\{getpass.getuser()}\\Music\\{name.replace(".mp4", "")}.mp3')
     video.close()
     time.sleep(5)
-    os.remove(f"{name}.mp4")
+    if not enter:
+        os.remove(f"{name}.mp4")
+    else:
+        os.remove(name)
